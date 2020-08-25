b5712f974c256a0ba635e022d1b9665c3748ff76
havidalbar <havidalbar@gmail.com>
2020-08-25 10:58:15 +0700

    change network limit

--- app/src/main/java/com/ublipi/questionAnswer/util/Constant.kt
+++ app/src/main/java/com/ublipi/questionAnswer/util/Constant.kt
@@ -1,7 +1,7 @@
 package com.ublipi.questionAnswer.util
 
 object Constant {
-    const val NETWORK_TIMEOUT = 60L
+    const val NETWORK_TIMEOUT = 120L
     const val BASE_URL = "http://10.0.2.2:8000"
     const val MY_ID = 1
 
